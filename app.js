0dc0ca8552ad811a4aac46e9b445772e7dce6479
reactiongears <reactiongears@gmail.com>
2015-09-08 19:10:57 -0700

    Added a custom service for the cityName

--- app.js
+++ app.js
@@ -17,20 +17,32 @@ weatherApp.config(function ($routeProvider) {
             controller: 'forecastController'
         });
 });
+//------------------------------------------------------------------------------------------------------------------------------------------
+//                                  Custom Service
+//------------------------------------------------------------------------------------------------------------------------------------------
+weatherApp.service('cityNameService', function(){
+    this.cityName = '';
+});
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 //                                  Home Page Controller
 //------------------------------------------------------------------------------------------------------------------------------------------
-weatherApp.controller('homeController', ['$scope', '$log', function ($scope, $log) {
+weatherApp.controller('homeController', ['$scope', '$log', 'cityNameService', function ($scope, $log, cityNameService) {
     'use strict';
     $log.info("You are on the Home Page");
+    
+    $scope.$watch('cityName', function () {
+        cityNameService.cityName = $scope.cityName;
+        $log.info($scope.cityName)
+    });
 }]);
 
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 //                                 Forcast Page Controller
 //------------------------------------------------------------------------------------------------------------------------------------------
-weatherApp.controller('forecastController', ['$scope', '$log', function ($scope, $log) {
+weatherApp.controller('forecastController', ['$scope', '$log', 'cityNameService', function ($scope, $log, cityNameService) {
     'use strict';
-    $log.info("You are on the Forecast Page");
+    $log.info("You are on the Forecast Page for city "+cityNameService.cityName);
+    $scope.cityName = cityNameService.cityName;
 }]);
